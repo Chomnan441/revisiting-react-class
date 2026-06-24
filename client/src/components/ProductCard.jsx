cb343c6df2ec392b4f85f7a1de0edc72512ce686
Chomnan <chomnan415@gmail.com>
2026-06-24 14:54:09 +0700

    refactor: update ProductCard props for consistency in ProductHighlight

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -5,8 +5,8 @@ import { ShoppingCartIcon } from "@heroicons/react/24/solid";
 function ProductCard(props) {
   return (
     <div className="bg-white p-4 shadow-md">
-      <img src={props.image} alt={props.alt} className="w-full mb-2" />
-      <h3 className="text-lg font-semibold mb-1">{props.title}</h3>
+      <img src={props.imgSrc} alt={props.productName} className="w-full mb-2" />
+      <h3 className="text-lg font-semibold mb-1">{props.productName}</h3>
       <div className="flex justify-between items-center">
         <p className="text-gray-600">{props.price}</p>
         <button className="mt-2 border border-blue-500 text-blue-500 hover:bg-blue-500 hover:text-white py-2 px-4 rounded flex items-center">
--- client/src/components/ProductHighlight.jsx
+++ client/src/components/ProductHighlight.jsx
@@ -7,21 +7,18 @@ function ProductHighlight() {
         <h2 className="text-2xl font-semibold mb-4">Featured Products</h2>
         <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
           <ProductCard
-            image="https://picsum.photos/id/231/200/200"
-            alt="The Lorem Ipsum for photos."
-            title="The Lorem Ipsum for photos."
+            imgSrc="https://picsum.photos/id/231/200/200"
+            productName="The Lorem Ipsum for photos."
             price="$99.99"
           />
           <ProductCard
-            image="https://picsum.photos/id/232/200/200"
-            alt="Wireless Bluetooth Earbuds"
-            title="Wireless Bluetooth Earbuds"
+            imgSrc="https://picsum.photos/id/232/200/200"
+            productName="Wireless Bluetooth Earbuds"
             price="$79.99"
           />
           <ProductCard
-            image="https://picsum.photos/200/200"
-            alt="Product 4"
-            title="Stylish Cool Sunglasses in The World"
+            imgSrc="https://picsum.photos/200/200"
+            productName="Stylish Cool Sunglasses in The World"
             price="$79.99"
           />
         </div>
